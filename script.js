ca120661df2f4825268318fc78eb8589f7810b05
Mehedi Hassan <howlader.mehedihassan@gmail.com>
2022-08-23 00:05:23 +0600

    complete all the necessary functions

---
+++ script.js
@@ -0,0 +1,40 @@
+import data from "./data.js";
+
+const passwordElement = document.getElementById("password");
+const regenerateButtonElement = document.getElementById("password-generator-button");
+const copyButtonElement = document.getElementById("copy-option-button");
+const messageElement = document.querySelector(".message");
+
+const { capitalCaseAlphabets, smallCaseAlphabets, symbols } = data;
+const TOTAL_ALPHABETIC_CHARACTERS = 26;
+const TOTAL_SYMBOLIC_CHARACTERS = 30;
+
+// Generates a password and inserts the value inside input element
+function generatePassword() {
+    let password = "";
+    let iterator = 1;
+    while (iterator <= 16) {
+        if (iterator % 3 === 0) password += capitalCaseAlphabets[Math.floor(Math.random() * TOTAL_ALPHABETIC_CHARACTERS)];
+        else if (iterator % 5 === 0) password += smallCaseAlphabets[Math.floor(Math.random() * TOTAL_ALPHABETIC_CHARACTERS)];
+        else password += symbols[Math.floor(Math.random() * TOTAL_SYMBOLIC_CHARACTERS)];
+
+        ++iterator;
+    }
+
+    passwordElement.value = password;
+}
+
+function copyPasswordToClipBoard() {
+    navigator.clipboard.writeText(passwordElement.value);
+
+    messageElement.style.display = "block";
+    messageElement.innerText = "Copied to your clipboard";
+    setTimeout(() => {
+        messageElement.style.display = "none";
+    }, 3000);
+}
+
+generatePassword();
+
+regenerateButtonElement.addEventListener("click", generatePassword);
+copyButtonElement.addEventListener("click", copyPasswordToClipBoard);
