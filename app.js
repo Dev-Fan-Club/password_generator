630cd9087f61e62bfec149e8d839905230746bdf
alamin40 <alamin40>
2022-08-22 22:59:47 +0600

    added bootstrap, two funtion in js

---
+++ app.js
@@ -0,0 +1,23 @@
+function generatePass() {
+
+    const passField = document.getElementById('show-pass');
+      var password = '';
+      var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()';
+      for (i=0;i<16;i++) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        password += chars.charAt(c);
+      }
+      if(password.length == 16){
+         passField.value = password;
+      }
+ 
+}
+
+function copyPassword() {
+   let copyText = document.getElementById('show-pass');
+   copyText.select();
+   document.execCommand("copy");  
+};
+
+
+
